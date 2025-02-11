517f486cf9fb3e7bac90f0f25f3411e714d18d58
OrKaraz <50598355+OrKaraz@users.noreply.github.com>
2025-02-11 09:37:56 +0100

    Create libCOM.hpp

---
+++ COM/libCOM.hpp
@@ -0,0 +1,21 @@
+/*
+ * libCOM.hpp
+ *
+ *  Created on: 27 janv. 2025
+ *      Author: david
+ */
+
+#ifndef INCLUDE_LIBCOM_HPP_
+#define INCLUDE_LIBCOM_HPP_
+
+// décommenter les lignes nécessaires
+//#define SPIA
+#define SPIB
+
+#include <msp430.h>
+
+namespace COM {
+    void init();
+};
+
+#endif /* INCLUDE_LIBCOM_HPP_ */
